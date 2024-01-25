395b08601c411c7ff51152200e13ed061bff660b
Charlles Abreu <charlles@redesignscience.com>
2024-01-25 08:53:25 -0500

    Changed installation configs

--- python/setup.py
+++ python/setup.py
@@ -1,20 +1,20 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 import os
-import sys
 import platform
 
 openmm_dir = '@OPENMM_DIR@'
-customcppforces_header_dir = '@PLUGIN_HEADER_DIR@'
-customcppforces_library_dir = '@PLUGIN_LIBRARY_DIR@'
+header_dir = '@PLUGIN_HEADER_DIR@'
+library_dir = '@PLUGIN_HEADER_DIR@'
 
 # setup extra compile and link arguments on Mac
-extra_compile_args = ['-std=c++11']
+extra_compile_args = ['-std=c++17']
 extra_link_args = []
 
 if platform.system() == 'Darwin':
     extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-    extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-Wl', '-rpath', openmm_dir+'/lib']
+    extra_link_args += [
+        '-stdlib=libc++', '-mmacosx-version-min=10.7', '-Wl', '-rpath', openmm_dir+'/lib'
+    ]
 
 os.environ['CC'] = '@CMAKE_C_COMPILER@'
 os.environ['CXX'] = '@CMAKE_CXX_COMPILER@'
@@ -23,15 +23,15 @@ extension = Extension(
     name='_customcppforces',
     sources=['CustomCPPForcesWrapper.cpp'],
     libraries=['OpenMM', 'CustomCPPForces'],
-    include_dirs=[os.path.join(openmm_dir, 'include'), customcppforces_header_dir],
-    library_dirs=[os.path.join(openmm_dir, 'lib'), customcppforces_library_dir],
+    include_dirs=[os.path.join(openmm_dir, 'include'), header_dir],
+    library_dirs=[os.path.join(openmm_dir, 'lib'), library_dir],
     extra_compile_args=extra_compile_args,
     extra_link_args=extra_link_args,
 )
 
 setup(
     name='customcppforces',
-    version='@CMAKE_PROJECT_VERSION@',
+    version='@PROJECT_VERSION@',
     py_modules=['customcppforces'],
     ext_modules=[extension],
 )
